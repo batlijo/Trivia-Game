9bd130c616f47c6fdeb8b79946be31033d7776e3
Batlijo <batlijoselevitz@gmail.com>
2017-10-16 14:26:04 -0700

    pseudocode and questions

---
+++ assets/javascript/apps.js
@@ -0,0 +1,33 @@
+/* TO DO LIST
+- HTML
+	-div multiple choice q buttons
+		each has a name,id,value
+	- write down multiple choice questions in body
+- JAVASCRIPT
+	- Write 3 multiple choice questions
+		-Create a function to load all questions
+		-Do I have to create 3 arrays for 3 questions?
+	- Call a function to read in question and output them to screen
+	- 3 different types of buttons
+		- start button
+		- multiple choice answers buttons
+		- done button
+- JQUERY
+	- select a multiple choice per question x3 or x12 buttons?
+		-make on click button, do I make 4 buttons for each question 
+		 or is there a way to loop this?
+		
+- TIMERS
+	-setIntervalMethod
+	-setTimeOutMethod
+	- create countdown timer
+- CSS
+	-text-align: left
+	-position: relative
+-- Other QUESTIONS:
+	- How to make page reveal # of correct questions answered right/wrong
+	without making an alert??
+	- what is document.getElementById?
+	- How is dom manipulated to different layout structure through out game
+	 ie. start button, countdown timer with questions, results page?
+
