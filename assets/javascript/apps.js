5d65becd8778b5c74d8f2ce7dd32e62fae4bbae4
Batlijo <batlijoselevitz@gmail.com>
2017-10-19 16:06:31 -0700

    current working files

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,42 +1,45 @@
 $(document).ready(function(){
-/* TO DO LIST
-- HTML
-	-div multiple choice q buttons x
-		each has a name,id,value x
-	-How do I add the question? is this in javascript? how??
-
-- JAVASCRIPT
-	- Write 3 multiple choice questions x
-		-Create a function to load all questions x
-	- Call a function to read in question and output them to screen
-*/
 var image = document.getElementById("image");
-var text ="<h1> Roaring '20s Basic Trivia</h1>"
+var text ="<h1> Roaring '20s Basic Trivia</h1>";
 var div1=
 
 function loadQuestions(){
-	document.getElementById("questions").innerHTML="<body>"
+	var radioButton;
+	document.getElementById("questions").innerHTML="";
+	for(var i =0; i < quiz[currentQuestion]["choices"].length; i++){
+		radioButton = document.createElement("input");
+		radioButton.type = "radio";
+		radioButton.name= "quiz"+currentQuestion;
+		radioButton.id="choice"+ (i+1);
+		if (radioAnswer[currentQuestion] === i)
+		{
+			radioButton.checked= true;
+		}
+	radioButton.value = quiz[currentQuestion]["choices"][i];
+	}
 } 
+console.log(loadQuestions);
 
 var questions = [
 	{
-		question1: 'The Great Gatsby is a novel from what year?',
+		question1: "The Great Gatsby is a novel from what year?",
 		answer1: "1925",
 		answer2: "1927",
 		answer3: "1954."
 	},
 	{
-		question2: 'The Great Gatsby was written by who?',
+		question2: "The Great Gatsby was written by who?",
 		answer1: "Ernest Hemingway",
-		answer2: "F.Scott Fitzgerald,"
+		answer2: "F.Scott Fitzgerald",
 		answer3: "Maya Angelou."
 	},
 	{
-		question3: 'The 1920s era are also known as what?',
+		question3: "The 1920s era are also known as what?",
 		answer1: "roaring 20s",
 		answer2: "prohibition era",
 		answer3: "the decade of oh lala."
 	},
+console.log(questions)
 ];
 
 $('#start-button').on('click', function(){
@@ -45,62 +48,74 @@ $('#start-button').on('click', function(){
 		var $answers = $("<h4>" + questions[i].answer1 + "</h4>");
 		$("#questions").append($question);
 		$("#questions").append($answers);
+		console.log(start-button);
 
 		// <input type="radio" name="gender" value="male"> Male<br>
-
-
 	}
 });
 
+$('#done-button').on('click', function(){
+	// for(let i = 0; i < questions.length;i++){
+		console.log(done-button);
+	
+// WRITE showScore function()	
+	questionCount = answers.length;
+	correct = 0;
+	incorrect = 0;
+	for (i=0; i<questionCount; i++) {
+		if(useranswers[i] === answers [i])
+			correct++;
+		else
+			incorrect++;
+		}
+	})
+});
 
+// TIMERS
+$("#display").text("00:00");
+// $("#display").text(converted);
 
-/* - JQUERY
-	- select a multiple choice per question ?
-	 		-make on click button for the radio buttons?
- 	JQuery Button Function */
-// $("#start-button").on("click", function (){
-// 	console.log(button);
- });
+setTimeOut(timeUp, 1000);
+var limit = 1000;
+var intervals = 0;
+var totalTime = new Date ();
+var startTime = new Date ();
 
- // JQuery object for button
- // console.log(this);
-
- /* - TIMERS
-	-setIntervalMethod x
- 	-setTimeOutMethod, when do you use this?
- 	- create countdown timer x
-	THE TIMER 
- Our stopwatch object */
  var stopwatch = {
 	time:0,
  	reset: function () {
  		stopwatch.time = 0;
+ 		console.log(stopwatch);
  	},
  	start: function(){
- 	//DONE: use setInterval to start the count here and set the clock to running.
- 	if (!clockRunning) {
- 		intervalId = setInterval(stopwatch.count, 1000);
-		clockRunning = true
+ 		if (!clockRunning) {
+	 		intervalId = setInterval(stopwatch.count, 1000);
+			clockRunning = true
  		}
- 	},
- 	stop: function (){
+ 		console.log(clockRunning);
+ 	}
+};
+
+
+/* 
+function run() {
+	intervalId = setInterval(decrement, 1000);
+
+ 
+	stop: function (){
  		//use clearInterval to stop the count here
  		if (distance < 0) {
     clearInterval(x);
     document.getElementById("display").innerHTML = "TIME'S UP BUDDY";
-  }
-}, 1000);
- 	},
- 	recordLap: function() {
- 		var converted = stopwatch.timeConverter(stopwatch.time);
- 		console.log(converted);
- 		// use this variable to show the converted time
- 		$("#display").text(converted);
- 	},
+  } 1000
+	
 
+
+
+It says this function below is an unexpected identifier...WHYYYYYY
  	timeConverter: function(t){
- 		var minutes = Math.floor(t/60);
- 		var seconds = t - (minutes = 60);
+ 	  var minutes = Math.floor(t/60);
+ 	  var seconds = t - (minutes = 60);
  		if (seconds < 10){
  			seconds = "0" + seconds;
  		}
@@ -110,23 +125,14 @@ $('#start-button').on('click', function(){
  			minutes = "0" + minutes;
  		}
  		return minutes + ":" + seconds;
- 	}
+ 		}
+ 	},
 };
 
-/*
-- CSS
-	-text-align: left
-	-position: relative
--- Other QUESTIONS:
-	- How to make page reveal # of correct questions answered right/wrong
-	without making an alert??
-	- what is document.getElementById?
-	- How is dom manipulated to different layout structure through out game
-	 ie. start button, countdown timer with questions, results page? */
-
-
-/* 		radio buttons html
+ 		radio buttons html
 		True
 		<input type="radio" name="questions" value=""> </br>
 		False
 		<input type="radio" name="questions" value="">
+
+*/
