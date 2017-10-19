ca39cc28f30ef1d000088d407f4e4becdb35e497
Batlijo <batlijoselevitz@gmail.com>
2017-10-19 10:55:51 -0700

    working on css ish

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,13 +1,13 @@
+$(document).ready(function(){
 /* TO DO LIST
 - HTML
 	-div multiple choice q buttons x
 		each has a name,id,value x
 	-How do I add the question? is this in javascript? how??
-	
+
 - JAVASCRIPT
 	- Write 3 multiple choice questions x
 		-Create a function to load all questions x
-		-Do I have to create 3 arrays with 4 string for the 3 questions? Do I need to loop and do if else statements?
 	- Call a function to read in question and output them to screen
 */
 var image = document.getElementById("image");
@@ -15,93 +15,102 @@ var text ="<h1> Roaring '20s Basic Trivia</h1>"
 var div1=
 
 function loadQuestions(){
-	document.getElementById("questions").innerHTML="<h1>"
-} // 
+	document.getElementById("questions").innerHTML="<body>"
+} 
 
-// MAKE arrays with question answer properties, assign it in the correct answer property
-var question1 = ["A","B",]; <!--HOW DO YOU ADD VALUE TO THE VARIABLE AND STRINGS, can you do that?-->
-var length = question1.length
-console.log(length);
-// if click on B, increment correct answer by 1 else if click on A,C,D increment wrong answer by1
+var questions = [
+	{
+		question1: 'The Great Gatsby is a novel from what year?',
+		answer1: "1925",
+		answer2: "1927",
+		answer3: "1954."
+	},
+	{
+		question2: 'The Great Gatsby was written by who?',
+		answer1: "Ernest Hemingway",
+		answer2: "F.Scott Fitzgerald,"
+		answer3: "Maya Angelou."
+	},
+	{
+		question3: 'The 1920s era are also known as what?',
+		answer1: "roaring 20s",
+		answer2: "prohibition era",
+		answer3: "the decade of oh lala."
+	},
+];
 
-for (var i=0; i < question1.length; i++){
-	console.log(question1[i]);
+$('#start-button').on('click', function(){
+	for(let i = 0; i < questions.length;i++){
+		var $question = $("<input type='radio' name='separate_name' value='someValue'>" + questions[i].question1 + "</input>")
+		var $answers = $("<h4>" + questions[i].answer1 + "</h4>");
+		$("#questions").append($question);
+		$("#questions").append($answers);
+
+		// <input type="radio" name="gender" value="male"> Male<br>
+
+
 	}
-var question2 = ["A","B",];
-var length = question1.length
-console.log(length);
-// if click on D, increment correct answer by 1 else if click on A,B,C, increment wrong answer by1
-
-for (var i=0; i<question2.length; i++){
-	console.log(question2[i]);
-}
-
-var question3 = ["A","B",]; 
-var length = question3.length
-console.log(length);
-
-for (var i=0; i<question3.length; i++){
-	console.log(question3[i]);
-}
-// if click on A, increment correct answer by 1 else if click on B,C,D increment wrong answer by1
-
-/*
-	- 3 different types of buttons
-		- start button
-		- multiple choice answers buttons, 12 total>
-		- done button is responsible for checking correct answers. How do we do this?
-- JQUERY
-	- select a multiple choice per question ?
-		-make on click button for the radio buttons?
-	JQuery Button Function */
-$("#start-button").on("click", function (){
-	console.log(button);
 });
-// JQuery object for button
-console.log(this);
 
-		
-/* - TIMERS
+
+
+/* - JQUERY
+	- select a multiple choice per question ?
+	 		-make on click button for the radio buttons?
+ 	JQuery Button Function */
+// $("#start-button").on("click", function (){
+// 	console.log(button);
+ });
+
+ // JQuery object for button
+ // console.log(this);
+
+ /* - TIMERS
 	-setIntervalMethod x
-	-setTimeOutMethod, when do you use this?
-	- create countdown timer x
-	THE TIMER
-Our stopwatch object */
-var stopwatch = {
+ 	-setTimeOutMethod, when do you use this?
+ 	- create countdown timer x
+	THE TIMER 
+ Our stopwatch object */
+ var stopwatch = {
 	time:0,
-	reset: function () {
-		stopwatch.time = 0;
-	},
-	start: function(){
-	//DONE: use setInterval to start the count here and set the clock to running.
-	if (!clockRunning) {
-		intervalId = setInterval(stopwatch.count, 1000);
+ 	reset: function () {
+ 		stopwatch.time = 0;
+ 	},
+ 	start: function(){
+ 	//DONE: use setInterval to start the count here and set the clock to running.
+ 	if (!clockRunning) {
+ 		intervalId = setInterval(stopwatch.count, 1000);
 		clockRunning = true
-		}
-	},
-	stop: function (){
-		//use clearInterval to stop the count here
-	},
-	recordLap: function() {
-		var converted = stopwatch.timeConverter(stopwatch.time);
-		console.log(converted);
-		// use this variable to show the converted time
-		$("#display").text(converted);
-	},
+ 		}
+ 	},
+ 	stop: function (){
+ 		//use clearInterval to stop the count here
+ 		if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("display").innerHTML = "TIME'S UP BUDDY";
+  }
+}, 1000);
+ 	},
+ 	recordLap: function() {
+ 		var converted = stopwatch.timeConverter(stopwatch.time);
+ 		console.log(converted);
+ 		// use this variable to show the converted time
+ 		$("#display").text(converted);
+ 	},
 
-	timeConverter: function(t){
-		var minutes = Math.floor(t/60);
-		var seconds = t - (minutes = 60);
-		if (seconds < 10){
-			seconds = "0" + seconds;
-		}
-		if (minutes === 0){
-			minutes = "00";
+ 	timeConverter: function(t){
+ 		var minutes = Math.floor(t/60);
+ 		var seconds = t - (minutes = 60);
+ 		if (seconds < 10){
+ 			seconds = "0" + seconds;
+ 		}
+ 		if (minutes === 0){
+ 			minutes = "00";
 		}else if (minutes < 10){
-			minutes = "0" + minutes;
-		}
-		return minutes + ":" + seconds;
-	}
+ 			minutes = "0" + minutes;
+ 		}
+ 		return minutes + ":" + seconds;
+ 	}
 };
 
 /*
@@ -115,3 +124,9 @@ var stopwatch = {
 	- How is dom manipulated to different layout structure through out game
 	 ie. start button, countdown timer with questions, results page? */
 
+
+/* 		radio buttons html
+		True
+		<input type="radio" name="questions" value=""> </br>
+		False
+		<input type="radio" name="questions" value="">
