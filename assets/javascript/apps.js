5fe5981cb9f585d218039b69d0c2a7558bc37405
Batlijo <batlijoselevitz@gmail.com>
2017-10-19 21:42:23 -0700

    files up to date

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -3,8 +3,7 @@ var image = document.getElementById("image");
 var text ="<h1> Roaring '20s Basic Trivia</h1>";
 var div1=
 
-
-function loadQuestions(quiz){
+function loadQuestions(){
 	var radioButton;
 	document.getElementById("quiz").innerHTML="";
 	console.log(radioButton);
@@ -12,7 +11,7 @@ function loadQuestions(quiz){
 		radioButton = document.createElement("input");
 		radioButton.type = "radio";
 		radioButton.name= "quiz"+currentQuestion;
-		radioButton.id="choice"+ (i+1);
+		radioButton.id="choices"+ (i+1);
 		if (radioAnswer[currentQuestion] === i)
 		{
 			radioButton.checked= true;
@@ -20,7 +19,7 @@ function loadQuestions(quiz){
 	radioButton.value = quiz[currentQuestion]["choices"][i];
 	}
 } 
-//console.log(loadQuestions);
+//console.log(loadQuestions); for some reason this gives me an error message in console
 
 var quiz = [
 	{
@@ -48,7 +47,7 @@ $('#start-button').on('click', function(){
 	for(let i = 0; i < quiz.length;i++){
 		var question = $("<input type='radio' name='separate_name' value='someValue'>" + quiz[i] + "</input>")
 		var answers = $("<h3>" + quiz[i] + "</h3>");
-		$("#quiz").append($question);
+		$("#quiz").append($quiz);
 		$("#quiz").append($answers);
 		console.log(start-button);
 
@@ -57,14 +56,19 @@ $('#start-button').on('click', function(){
 });
 
 $('#done-button').on('click', function(){
-	// for(let i = 0; i < questions.length;i++){
-		console.log(done-button);
+	for(let i = 0; i < quiz.length;i++){
+	}
+//		console.log(done-button);
 	
-// WRITE showScore function()	
-	questionCount = answers.length;
+// WRITE showScore function()
+var answers= {
+	"one": "a",
+	"two": "b"
+}
+	quizCount = answers.length;
 	correct = 0;
 	incorrect = 0;
-	for (i=0; i<questionCount; i++) {
+	for (i=0; i<quizCount; i++) {
 		if(useranswers[i] === answers [i])
 			correct++;
 		else
@@ -73,86 +77,9 @@ $('#done-button').on('click', function(){
 	})
 });
 
+//Timer
 var count = 60, timer = setInterval(function () {
     $(".display").html(count--);
     if (count === 0) clearInterval(timer);
         }, 1000);
 
-/* TIMERS
-
-function startTime(duration, display) {
-	var timer = duration, minutes, seconds;
-	setInterval(function () {
-		minutes = parseInt(timer / 60, 10);
-		seconds = parseInt(timer % 60, 10);
-		$(".display").text(timer)
-	})
-}
-
-
-
-
-$("#display").text(converted);
- var setTimeout = 0;
-
- var limit = 1000;
-var intervals = 0;
-var totalTime = new Date ();
-var startTime = new Date ();
-
-
-var stopwatch = {
-	time:0,
- 	reset: function () {
- 		stopwatch.time = 0;
- 		console.log(stopwatch);
- 	},
- 	start: function(){
- 		if (!clockRunning) {
-	 		intervalId = setInterval(stopwatch.count, 1000);
-			clockRunning = true
- 		}
- 		console.log(clockRunning);
- 	}
-};
-
-
-
-function run() {
-	intervalId = setInterval(decrement, 1000);
-
- 
-	stop: function (){
- 		//use clearInterval to stop the count here
- 		if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("display").innerHTML = "TIME'S UP BUDDY";
-  } 1000
-	
-
-
-
-It says this function below is an unexpected identifier...WHYYYYYY
- 	timeConverter: function(t){
- 	  var minutes = Math.floor(t/60);
- 	  var seconds = t - (minutes = 60);
- 		if (seconds < 10){
- 			seconds = "0" + seconds;
- 		}
- 		if (minutes === 0){
- 			minutes = "00";
-		}else if (minutes < 10){
- 			minutes = "0" + minutes;
- 		}
- 		return minutes + ":" + seconds;
- 		}
- 	},
-};
-
- 		radio buttons html
-		True
-		<input type="radio" name="questions" value=""> </br>
-		False
-		<input type="radio" name="questions" value="">
-
-*/
